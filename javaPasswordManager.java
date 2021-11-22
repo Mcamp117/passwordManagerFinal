a212a728996777662a773a1283103cdb42ed8a70
Kai <kai.howard@evsck12.com>
2021-11-22 14:06:48 -0600

    randomized Password Generator

--- javaPasswordManager.java
+++ javaPasswordManager.java
@@ -1,19 +1,35 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class javaPasswordManager {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
         
         ArrayList <Boolean> reqCheckList = new ArrayList<Boolean>();
         ArrayList <Integer> specialCharacters = new ArrayList<Integer>();
         ArrayList <String> passwordList = new ArrayList<String>();
-        char[] lowerAlphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-        char[] upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-        char[] symbols = "!\"#$%'()*".toCharArray();
+
+        int loginTries = 5;
+
+        ArrayList <String> category = new ArrayList<String>();
+        category.add("home");
+        category.add("work");
+        category.add("entertainment");
+        category.add("bills");
+
+        randomizer();
+
     }
 
     private static void randomizer() {
         Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        ArrayList <String> ui = new ArrayList<String>();
+        
+        //User says how many Cap & Low Letters, Nums, and Syms
         System.out.println("Input Number of Capital Letters: ");
         int  capitalLetterInput = sc.nextInt();
         System.out.println("Input Number of Lowercase Letters: ");
@@ -22,13 +38,35 @@ public class javaPasswordManager {
         int  numberInput = sc.nextInt();
         System.out.println("Input Number of Symbols: ");
         int  symbolInput = sc.nextInt();
-        String letterDigit = "";
+        String password = "";
+
         //adds random items to the final output
         for ( int count=0; count<capitalLetterInput; count++) {
-            letterDigit += 
+            password += (char)getRandInt(65,91);
         }
 
+        for ( int count=0; count<lowerLetterInput; count++) {
+            password += (char)getRandInt(97,122);
+        }
+
+        for ( int count=0; count<numberInput; count++) {
+            password += (char)getRandInt(48, 57);
+        }
+
+        int special = 0;
+        String symbolsDigit = "";
+        while (!(symbolInput == 0)) {
+            special = getRandInt(33,64);
+            if ((special == 33) || (special == 35) || (special == 36) || (special == 37) || (special == 38) || (special == 40) || (special == 41) || (special == 42) || (special == 64)) {
+                password += (char) special;
+                symbolInput -= 1;
+            }
+        }
+        System.out.println(password);
+        // ui.addAll(1, password);
     }
+
+
 
     private static void printArray(int[] listy){
         for (int i=0; i<listy.length;i++){
@@ -54,4 +92,7 @@ public class javaPasswordManager {
         }
     }
 
+    private static int getRandInt(int minimum, int maximum){
+        return ((int) (Math.random()*(maximum - minimum))) + minimum;
+    }
 }
